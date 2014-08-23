a06abc44825e5843dcb6e54a7bcce167e57b32d7
Ryan Thompson <service@aiwebsystems.com>
2014-08-22 23:28:57 -0500

    Initial commit.

---
+++ src/HelperTag.php
@@ -0,0 +1,61 @@
+<?php namespace Addon\Tag\Helper;
+
+use Streams\Core\Addon\TagAbstract;
+
+class HelperTag extends TagAbstract
+{
+    /**
+     * Allowed PHP functions.
+     *
+     * @var array
+     */
+    protected $allowedFunctions = [
+        'count',
+        'empty',
+        'explode',
+        'html_entity_decode',
+        'htmlentities',
+        'htmlspecialchars',
+        'htmlspecialchars_decode',
+        'implode',
+        'is_array',
+        'is_int',
+        'is_integer',
+        'is_string',
+        'ltrim',
+        'md5',
+        'nl2br',
+        'preg_match',
+        'preg_replace',
+        'rtrim',
+        'sprintf',
+        'str_replace',
+        'str_word_count',
+        'strip_tags',
+        'strpos',
+        'strtolower',
+        'strtoupper',
+        'substr',
+        'trim',
+        'ucfirst',
+        'ucwords',
+    ];
+
+    /**
+     * Hook into the call method to allow PHP functions.
+     *
+     * @param $method
+     * @param $arguments
+     * @return null|void
+     */
+    public function __call($method, $arguments)
+    {
+        if (in_array($method, $this->allowedFunctions) and function_exists($method)) {
+            return call_user_func_array($method, $this->attributes);
+        } elseif (method_exists($this, $method)) {
+            return call_user_func_array([$this, $method], $this->attributes);
+        }
+
+        return null;
+    }
+}
